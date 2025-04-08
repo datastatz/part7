d32f08f689d2bc5a79b3b138e745b84a3cc82f1b
scraper <155395474+datastatz@users.noreply.github.com>
2025-04-08 18:25:59 +0200

    finished untill 7.8

--- country-hook-main/src/App.jsx
+++ country-hook-main/src/App.jsx
@@ -18,30 +18,36 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) return
+
+    const fetchCountry = async () => {
+      try {
+        const response = await axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}?fullText=true`)
+        setCountry({ data: response.data, found: true })
+      } catch (error) {
+        setCountry({ found: false })
+      }
+    }
+
+    fetchCountry()
+  }, [name])
 
   return country
 }
 
 const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
+  if (!country) return null
+  if (!country.found) return <div>not found...</div>
 
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
+  const data = country.data
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <h3>{data.name.common}</h3>
+      <div>capital {data.capital[0]}</div>
+      <div>population {data.population}</div> 
+      <img src={data.flags.png} height='100' alt={`flag of ${data.name.common}`} />  
     </div>
   )
 }
